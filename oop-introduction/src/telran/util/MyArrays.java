372fb1892ee02342981ff7f0c5e798e2d381d4b0
eva-gor <evagor82@gmail.com>
2022-12-23 08:56:35 +0200

    HW#17: MyArrays - removeIf, removeRepeated, contains methods

--- oop-introduction/src/telran/util/MyArrays.java
+++ oop-introduction/src/telran/util/MyArrays.java
@@ -1,6 +1,9 @@
 package telran.util;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class MyArrays {
 
@@ -26,22 +29,79 @@ public class MyArrays {
 		T tmp = objects[i];
 		objects[i] = objects[j];
 		objects[j] = tmp;
-		
+
 	}
 
-	public static <T> int binarySearchObject (T[] arraySorted, T searchedValue, Comparator<T> comp) {		
+	public static <T> int binarySearchObject(T[] arraySorted, T searchedValue, Comparator<T> comp) {
 		int left = 0;
 		int right = arraySorted.length - 1;
 		int middle = arraySorted.length / 2;
-		while (left <= right && comp.compare(arraySorted[left], searchedValue)  != 0) {
-			if (comp.compare(searchedValue, arraySorted[middle])<= 0) {
+		while (left <= right && comp.compare(arraySorted[left], searchedValue) != 0) {
+			if (comp.compare(searchedValue, arraySorted[middle]) <= 0) {
 				right = middle - 1;
 			} else {
 				left = middle + 1;
 			}
 			middle = (left + right) / 2;
 		}
-		return left < arraySorted.length &&  comp.compare(arraySorted[left], searchedValue) == 0 ? left : -left - 1;
+		return left < arraySorted.length && comp.compare(arraySorted[left], searchedValue) == 0 ? left : -left - 1;
 	}
-	
+
+	public static <T> T[] filter(T[] array, Predicate<T> predicate) {
+		int counPredicate = getCountPredicate(array, predicate);
+		T[] res = Arrays.copyOf(array, counPredicate);
+		int index = 0;
+		for (T element : array) {
+			if (predicate.test(element)) {
+				res[index++] = element;
+			}
+		}
+		return res;
+	}
+
+	private static <T> int getCountPredicate(T[] array, Predicate<T> predicate) {
+		int res = 0;
+		for (T element : array) {
+			if (predicate.test(element)) {
+				res++;
+			}
+		}
+		return res;
+	}
+
+	public static <T> T[] removeIf(T[] array, Predicate<T> predicate) {
+		return filter(array, predicate.negate());
+	}
+
+	public static <T> T[] removeRepeated(T[] array) {
+		T[] res = Arrays.copyOf(array, array.length);
+		int i = 0;
+		while (i < res.length - 1) {
+			// right part after element i without duplicates of element i
+			T[] right = removeEqualsToValue(Arrays.copyOfRange(res, i + 1, res.length), res[i]); 
+			int rightLength = right.length;
+			if (i + 1 + rightLength != res.length) {
+				res = Arrays.copyOf(res, i + 1 + rightLength);
+				System.arraycopy(right, 0, res, i + 1, rightLength);
+			}
+			i++;
+		}
+		return res;
+	}
+
+	private static <T> T[] removeEqualsToValue(T[] array, T value) {
+		return removeIf(array, x -> x.equals(value));
+	}
+
+	/**
+	 * returns true if element equaled to pattern exists in array
+	 */
+	public static <T> boolean contains(T[] array, T pattern) {
+		boolean res = false;
+		int i = 0;
+		while (!res && i < array.length) {
+			res = array[i++].equals(pattern);
+		}
+		return res;
+	}
 }
--- oop-introduction/src/telran/util/test/MyArraysTest.java
+++ oop-introduction/src/telran/util/test/MyArraysTest.java
@@ -2,14 +2,22 @@ package telran.util.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Comparator;
+import java.util.function.Predicate;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import telran.util.EvenOddComparator;
-import telran.util.MyArrays;
+import static telran.util.MyArrays.*;
 import telran.util.StringsLengthComparator;
 
 class MyArraysTest {
+
+	Integer numbers[] = { 13, 2, -8, 47, 100, 10, 7 };
+	String[] strings = { "abcd", "lmn", "zz", "fghwm" };
+
+	Comparator<Integer> evenOddComparator = this :: evenOddCompare;
 
 	@Test
 	@Disabled
@@ -17,37 +25,101 @@ class MyArraysTest {
 		String[] strings = { "abcd", "lmn", "zz" };
 		String[] expected = { "zz", "lmn", "abcd" };
 
-		MyArrays.sort(strings, new StringsLengthComparator());
+		sort(strings, new StringsLengthComparator());
 		assertArrayEquals(expected, strings);
 
 	}
-	
+
 	@Test
+	@Disabled
 	void evenOddTest() {
 		Integer numbers[] = { 13, 2, -8, 47, 100, 10, 7 };
 		Integer expected[] = { -8, 2, 10, 100, 47, 13, 7 };
-		MyArrays.sort(numbers, new EvenOddComparator());
+		sort(numbers, evenOddComparator);
 		assertArrayEquals(expected, numbers);
 
 	}
-	
+
 	@Test
+	@Disabled
 	void binarySearchTest() {
 		Integer numbers[] = { 13, 2, -8, 47, 100, 10, 7 };
 		var compInt = new EvenOddComparator();
-		MyArrays.sort(numbers, compInt);
-		assertEquals(0, MyArrays.binarySearchObject(numbers, -8, compInt));
-		assertEquals(2, MyArrays.binarySearchObject(numbers, 10, compInt));
-		assertEquals(-8, MyArrays.binarySearchObject(numbers, 1, compInt));	// not in array
-		assertEquals(-4, MyArrays.binarySearchObject(numbers, 12, compInt));	// not in array
-		assertEquals(-6, MyArrays.binarySearchObject(numbers, 45, compInt));	// not in array
-		
+		sort(numbers, compInt);
+		assertEquals(0, binarySearchObject(numbers, -8, compInt));
+		assertEquals(2, binarySearchObject(numbers, 10, compInt));
+		assertEquals(-8, binarySearchObject(numbers, 1, compInt)); // not in array
+		assertEquals(-4, binarySearchObject(numbers, 12, compInt)); // not in array
+		assertEquals(-6, binarySearchObject(numbers, 45, compInt)); // not in array
+
 		String[] strings = { "abcd", "lmn", "zz" };
-		MyArrays.sort(strings, new StringsLengthComparator());
+		sort(strings, new StringsLengthComparator());
 		var compStr = new StringsLengthComparator();
-		assertEquals(0, MyArrays.binarySearchObject(strings, "zz", compStr));
-		assertEquals(2, MyArrays.binarySearchObject(strings, "abcd", compStr));
-		assertEquals(-1, MyArrays.binarySearchObject(strings, "a", compStr));	// not in array
-		assertEquals(-4, MyArrays.binarySearchObject(strings, "12345", compStr));	// not in array
+		assertEquals(0, binarySearchObject(strings, "zz", compStr));
+		assertEquals(2, binarySearchObject(strings, "abcd", compStr));
+		assertEquals(-1, binarySearchObject(strings, "a", compStr)); // not in array
+		assertEquals(-4, binarySearchObject(strings, "12345", compStr)); // not in array
+	}
+
+	@Test
+	@Disabled
+	void filterTest() {
+		int divider = 2;
+		String subStr = "m";
+		Predicate<Integer> predEven = t -> t % divider == 0;
+		Predicate<String> predSubstr = s -> s.contains(subStr);
+		String expectedStr[] = { "lmn", "fghwm" };
+		Integer expectedNumbers[] = { 2, -8, 100, 10 };
+
+		assertArrayEquals(expectedStr, filter(strings, predSubstr));
+		assertArrayEquals(expectedNumbers, filter(numbers, predEven));
+	}
+
+	@Test
+	void removeIfTest() {
+		int divider = 2;
+		String subStr = "m";
+		Predicate<Integer> predEven = t -> t % divider == 0;
+		Predicate<String> predSubstr = s -> s.contains(subStr);
+		Integer expectedNumbers[] = { 13, 47, 7 };
+		String expectedStr[] =  { "abcd", "zz" };
+		
+		assertArrayEquals(expectedStr, removeIf(strings, predSubstr));
+		assertArrayEquals(expectedNumbers, removeIf(numbers, predEven));
+	}
+	
+	@Test
+	void removeRepeatedTest() {
+		Integer[] numbersRepeatedValues = { 13, 13, 2, -8, -8, 47, 100, 100, 100, 10, 7, 7 , 13};
+		String[] stringsRepeatedValues = { "abcd", "lmn", "zz", "fghwm", "abcd", "zz" };
+		
+		assertArrayEquals(numbers, removeRepeated(numbersRepeatedValues));
+		assertArrayEquals(strings, removeRepeated(stringsRepeatedValues));
+	}
+	
+	@Test
+	void containsTest() {
+		assertTrue(contains(numbers, 13));
+		assertTrue(contains(numbers, -8));
+		assertTrue(contains(numbers, 7));
+		
+		assertTrue(contains(strings, "abcd"));
+		assertTrue(contains(strings, "zz"));
+		assertTrue(contains(strings, "fghwm"));
+		
+		assertFalse(contains(numbers, 1000));
+		assertFalse(contains(numbers, "-8"));
+		
+		assertFalse(contains(strings, "-8"));
+		assertFalse(contains(strings, "abc"));
+		assertFalse(contains(strings, "abcde"));
+	}
+	
+	int evenOddCompare(Integer o1, Integer o2){
+		int res = Math.abs(o1) % 2 - Math.abs(o2) % 2;
+	if (res == 0) {
+		res = o1 % 2 != 0 ? Integer.compare(o2, o1) : Integer.compare(o1, o2);
+	}
+	return res;
 	}
 }
