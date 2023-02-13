b7b734541037754cc6329438875c25e6c55c8201
eva-gor <evagor82@gmail.com>
2023-02-13 16:18:43 +0200

    HW #30 - in assumption of public access to the nodes of LinkedList

--- oop-introduction/src/telran/util/LinkedHashSet.java
+++ oop-introduction/src/telran/util/LinkedHashSet.java
@@ -1,132 +1,56 @@
 package telran.util;
 
 import java.util.Iterator;
-import java.util.NoSuchElementException;
+import telran.util.LinkedList.Node;
 
 public class LinkedHashSet<T> extends AbstractCollection<T> implements Set<T> {
-	private class Entry<T>{
-		public Entry<T> prev= null;
-		public Entry<T> next= null;
-		
-		public T obj= null;
-		
-	}
-	
-	private class LinkedHashSetIterator implements Iterator<T> {
-		Entry<T> current = head;
-		boolean flNext = false;
-		@Override
-		public boolean hasNext() {
-			
-			return current != null;
-		}
 
+	private class LinkedList4Hash<T> extends LinkedList<T> {
 		@Override
-		public T next() {
-			if(!hasNext()) {
-				throw new NoSuchElementException();
-			}
-			T res = current.obj;
-			current = current.next;
-			flNext = true;
-			return res;
-		}
-		@Override
-		public void remove() {
-			if(!flNext) {
-				throw new IllegalStateException();
-			}
-			Entry<T> removedNode = current == null ? tail : current.prev;
-			removeEntry(removedNode);
-			flNext = false;
-		}
+		void removeNode(Node<T> current) {
+			super.removeNode(current);
+			removeObj(current.obj);
 
-	}
-	
-	Entry<T> newEntry;
-	Map<T, Entry<T>> hashMap;
-	Entry<T> head;
-	Entry<T> tail;
-	
-	private void removeEntry(Entry<T> current) {
-		if (current == head) {
-			removeHead();
-		} else if (current == tail) {
-			removeTail();
-		} else {
-			removeMiddle(current);
 		}
-		hashMap.remove(current.obj);
-		
+	}
+
+	@SuppressWarnings("unchecked")
+	private void removeObj(Object obj) {
+		hashMap.remove((T) obj);
 		size--;
-		
 	}
-	
+
+	LinkedList4Hash<T> list;
+	Map<T, Node<T>> hashMap;
+
 	@Override
 	public boolean add(T element) {
 		boolean res = false;
-		
-		newEntry = new Entry<T>();
 		if (size == 0) {
-			hashMap = new HashMap<>();
-			head = tail = newEntry;
-		} 
-		newEntry.prev = tail;
-		newEntry.obj = element;
-		
-		res = hashMap.putIfAbsent(element, newEntry)== null ? true : false;
-		if (res) {
-			tail.next = newEntry;
-			tail = newEntry;
+			list = new LinkedList4Hash<>();
+			hashMap = new HashMap<T, LinkedList.Node<T>>();
+		}
+
+		if (!hashMap.containsKey(element)) {
+			list.add(element);
+			hashMap.put(element, list.tail);
 			size++;
-		}		
+			res = true;
+		}
 		return res;
 	}
 
 	@Override
 	public boolean remove(T pattern) {
-		Entry<T> entry = hashMap.get(pattern);
-		if (entry == null) {
-			return false;
+		boolean res = false;
+		if (hashMap.containsKey(pattern)) {
+			Node<T> node = hashMap.remove(pattern);
+			list.removeNode(node);
+			res = true;
 		}
-		if (entry == head) {
-			removeHead();
-		} else if (entry == tail) {
-			removeTail();
-		} else {
-			removeMiddle(entry);
-		}
-		hashMap.remove(pattern);
-		size--;
-		return true;
+		return res;
 	}
 
-	private void removeHead() {
-		if (head.next == null) {
-			head = tail = null;
-		} else {
-			Entry<T> next = head.next;
-			next.prev = null;
-			head = next;
-		}
-	}
-
-	private void removeMiddle(Entry<T> current) {
-		Entry<T> prev = current.prev;
-		Entry<T> next = current.next;
-		prev.next = next;
-		next.prev = prev;
-		
-	}
-
-
-
-	private void removeTail() {
-		Entry<T> prev = tail.prev;
-		prev.next = null;
-		tail = prev;
-		
-	}
 	@Override
 	public boolean contains(T pattern) {
 		return hashMap.containsKey(pattern);
@@ -134,11 +58,11 @@ public class LinkedHashSet<T> extends AbstractCollection<T> implements Set<T> {
 
 	@Override
 	public Iterator<T> iterator() {
-		return new LinkedHashSetIterator();
+		return list.iterator();
 	}
 
 	@Override
-	public T get(T pattern) {		
+	public T get(T pattern) {
 		return hashMap.get(pattern).obj;
 	}
 
